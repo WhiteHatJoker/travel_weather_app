bf9190984a3ddde54a94fff828a08040e7d4ea3f
whitehatjoker <rkhudoyberdiev@gmail.com>
2020-08-03 22:28:40 +0500

    setup api variables on backend

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,10 @@
 // Setup empty JS object to act as endpoint for all routes
 let projectData = {};
 
-const express = require('express');
+const geonamesApiUrl = "http://api.geonames.org/search?q=london&maxRows=1&type=json&username=rkhudoyberdiev"
 
+// Setup Express
+const express = require('express');
 const app = express();
 /* Middleware*/
 const bodyParser = require('body-parser');
@@ -11,16 +13,19 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
+// Configure adding variables from .env
+const dotenv = require('dotenv');
+dotenv.config();
+const geonamesUser = process.env.GEONAMES_USERNAME;
 // Initialize the main project folder
 app.use(express.static('dist'));
-
+// Require axios to get requests
 const axios = require('axios');
 
 const port = 8081;
 // Setup Server
 const listening = () => console.log(`Server is up and running on port ${port}`)
 const server = app.listen(port, listening);
-
 
 
 app.post('/addWeather', (req, res) => {
